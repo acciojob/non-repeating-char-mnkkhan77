fb975d8a1dc86eff33b39df4e352ef9da25a6a0e
Nasir Khan <37503821+mnkkhan77@users.noreply.github.com>
2025-03-13 20:38:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 function firstNonRepeatedChar(str) {
  // Write your code here
+    for (let idx = 0; idx < s.length; idx++){
+        if(s.indexOf(s[idx]) === s.lastIndexOf(s[idx])){
+            return idx
+        } else {
+            return -1
+        }
+    }
 }
 const input = prompt("Enter a string");
 alert(firstNonRepeatedChar(input)); 
